4ec21388388abfdc1108155ff7e0030700e8e7a7
Jakub Nesetril <jakub@gooddata.com>
2010-10-04 12:00:45 +0200

    Deployment config

--- Jakefile.js
+++ Jakefile.js
@@ -11,13 +11,22 @@ var run_command = function(info, command, callback) {
 
 desc('…blank…');
 task('default', [], function() {
-    sys.puts('Available targets: build, deploy, clean');
+    sys.puts('Available targets: build');
 }, true);
 
-desc('Prepare for git push');
+desc('Build Jekyll website, compress CSS');
 task('build', [], function() {
-    run_command('Compiling CSS from LESS...', 'lessc css/style.less | java -jar /usr/local/bin/yuicompressor.jar --type css > css/style.css', function() {
+    run_command('Building Jekyll site...', 'rm -Rf _site && jekyll && cat _site/css/style.css | java -jar /usr/local/bin/yuicompressor.jar --type css > _site/css/style.css', function() {
         sys.puts('done.');
         complete();
     });
 }, true);
+
+desc('Push website to developer-beta');
+task('deploy', [], function() {
+    run_command('Uploading website...', '$(cd _site/ && tar czf - . | ssh root@jn.users.getgooddata.com \'cat | tar xzf test.tgz -C /opt/devsite/\')', function() {
+        sys.puts('done.');
+        complete();
+    });
+}, true);
+
